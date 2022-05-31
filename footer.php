1ce31b919e5b427af7826f8cf3ad08b9bdc31448
Anastasssss <90850168+Anastasssss@users.noreply.github.com>
2022-06-01 00:16:26 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer fixed-bottom text-center text-lg-start bg-light text-muted">
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        Я тут футер сделала не знаю, что вставить. Пусть будет котик
+        Кис-кис, кис-кис, я - котик, ты - котик 
         <img src="./cat.jpg" alt="" width="50px" height="50px">
     </div>
 </footer>
